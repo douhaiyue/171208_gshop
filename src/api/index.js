eecd07ac0fb144e56b6e55380d13f194f6256f51
douhaiyue <572271358@qq.com>
2019-11-13 16:43:17 +0800

    init gshop

---
+++ src/api/index.js
@@ -0,0 +1,5 @@
+/*ajax请求函数*/
+import axios from 'axios'
+export default function ajax(url,data={},type='GET'){
+
+}
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
+
+/*入口JS*/
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 
-Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    el:'#app',
+   render: h => h(App),
+    router
+})
